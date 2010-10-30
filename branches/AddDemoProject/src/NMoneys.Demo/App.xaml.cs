9b9373212b1c0ea62e101a2083c3f439ceacca8c
danielgonnet <danielgonnet@8c90c430-1bf3-a179-f29b-9acbecd64b06>
2010-10-30 22:46:41 +0000

    Simplest Grid

---
+++ branches/AddDemoProject/src/NMoneys.Demo/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace NMoneys.Demo
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+	}
+}
--- branches/AddDemoProject/src/NMoneys.Demo/Properties/AssemblyInfo.cs
+++ branches/AddDemoProject/src/NMoneys.Demo/Properties/AssemblyInfo.cs
@@ -1,15 +1,17 @@
 ﻿using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Windows;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NMoneys")]
+[assembly: AssemblyTitle("NMoneys.Demo")]
 [assembly: AssemblyDescription("Implementation of the Money Value Object in .Net")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NMoneys")]
+[assembly: AssemblyProduct("NMoneys.Demo")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -18,8 +20,25 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6008a10f-70bd-400b-8ebb-112069622351")]
+//In order to begin building localizable applications, set 
+//<UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
+//inside a <PropertyGroup>.  For example, if you are using US english
+//in your source files, set the <UICulture> to en-US.  Then uncomment
+//the NeutralResourceLanguage attribute below.  Update the "en-US" in
+//the line below to match the UICulture setting in the project file.
+
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+
+
+[assembly: ThemeInfo(
+	ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+	//(used if a resource is not found in the page, 
+	// or application resource dictionaries)
+	ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+	//(used if a resource is not found in the page, 
+	// app, or any theme specific resource dictionaries)
+)]
+
 
 // Version information for an assembly consists of the following four values:
 //
@@ -33,7 +52,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.1.0")]
 [assembly: AssemblyFileVersion("1.0.1.0")]
-
-[assembly: InternalsVisibleTo("NMoneys.Tests")]
-[assembly: InternalsVisibleTo("NMoneys.Tools")]
-[assembly: System.Windows.Markup.XmlnsDefinition(NMoneys.Serialization.Data.NAMESPACE, "NMoneys")]
