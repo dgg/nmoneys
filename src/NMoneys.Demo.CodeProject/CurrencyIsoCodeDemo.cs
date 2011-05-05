fc3f5fc51ba53412e04314d7de6d5e02dfd32e93
danielgonnet@gmail.com <danielgonnet@gmail.com@8c90c430-1bf3-a179-f29b-9acbecd64b06>
2011-05-05 11:08:43 +0000

    solved the whole rename folder mess

---
+++ src/NMoneys.Demo.CodeProject/CurrencyIsoCodeDemo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using NMoneys.Demo.CodeProject.Support;
+using NUnit.Framework;
+
+namespace NMoneys.Demo.CodeProject
+{
+	[TestFixture]
+	public class CurrencyIsoCodeDemo
+	{
+		[Test]
+		public void currency_codes_are_modeled_as_enums_named_after_its_ISO_alphabetic_code()
+		{
+			CurrencyIsoCode usDollars = CurrencyIsoCode.USD;
+			CurrencyIsoCode euro = CurrencyIsoCode.EUR;
+			CurrencyIsoCode danishKrona = CurrencyIsoCode.DKK;
+			CurrencyIsoCode noCurrency = CurrencyIsoCode.XXX;
+		}
+
+		[Test]
+		public void currency_codes_have_their_ISO_numeric_value()
+		{
+			Assert.That((short)CurrencyIsoCode.USD, Is.EqualTo(840));
+			Assert.That((short)CurrencyIsoCode.EUR, Is.EqualTo(978));
+			Assert.That((short)CurrencyIsoCode.DKK, Is.EqualTo(208));
+			Assert.That((short)CurrencyIsoCode.XXX, Is.EqualTo(999));
+		}
+
+		[Test]
+		public void less_common_currencies_are_also_modeled_as_long_as_they_are_approved_by_ISO()
+		{
+			CurrencyIsoCode platinum = CurrencyIsoCode.XPT;
+			CurrencyIsoCode yemeniRial = CurrencyIsoCode.YER;
+		}
+
+		[Test]
+		public void recently_deprecated_currencies_are_also_present()
+		{
+			var estonianKroon = CurrencyIsoCode.EEK;
+			Assert.That(estonianKroon.AsAttributeProvider(), Has.Attribute<ObsoleteAttribute>());
+		}
+	}
+}
