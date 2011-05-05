db41e88db9cbc005c7424660473e796e62d38fd9
danielgonnet@gmail.com <danielgonnet@gmail.com@8c90c430-1bf3-a179-f29b-9acbecd64b06>
2011-05-05 10:53:33 +0000

    renamed folder

---
+++ trunk/src/NMoneys.Demo.CodeProject/Support/CurrencyIsoCode.Extensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using NMoneys;
+
+namespace NMoneys_Demo.CodeProject.Support
+{
+	internal static class CurrencyIsoCodeExtensions
+	{
+		internal static ICustomAttributeProvider AsAttributeProvider(this CurrencyIsoCode code)
+		{
+			return typeof(CurrencyIsoCode).GetField(code.ToString());
+		}
+	}
+}
