7f4a7b99b95e844f6bead88e9793839261b02d38
danielgonnet@gmail.com <danielgonnet@gmail.com@8c90c430-1bf3-a179-f29b-9acbecd64b06>
2011-05-16 15:00:44 +0000

    Tagged 1.5.0.0

---
+++ tags/1.5.0.0/src/NMoneys/Support/TokenizedValue.cs
@@ -0,0 +1,17 @@
+﻿namespace NMoneys.Support
+{
+	internal abstract class TokenizedValue
+	{
+		public static readonly char Tokenizer = ' ';
+
+		protected static string[] split(string value)
+		{
+			return value.Split(Tokenizer);
+		}
+
+		protected static string join(string[] values)
+		{
+			return string.Join(Tokenizer.ToString(), values);
+		}
+	}
+}
