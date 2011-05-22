722dd47f4601e02f432ed565df0ddf8d1768ad7c
danielgonnet@gmail.com <danielgonnet@gmail.com@8c90c430-1bf3-a179-f29b-9acbecd64b06>
2011-05-22 18:09:10 +0000

    branching to spike being able to provide currency exchage framework

---
+++ tags/NMoneys.Exchange/src/NMoneys.Tests/CustomConstraints/MoneyConstraint.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace NMoneys.Tests.CustomConstraints
+{
+	internal class MoneyConstraint : CustomConstraint<Money>
+	{
+		public MoneyConstraint(decimal amount, Currency currency)
+		{
+			_inner = new AndConstraint(
+				new PropertyConstraint<Money>(m => m.Amount, Is.EqualTo(amount)),
+				new PropertyConstraint<Money>(m => m.CurrencyCode, Is.EqualTo(currency.IsoCode)));
+		}
+
+		protected override bool matches(Money current)
+		{
+			return _inner.Matches(current);
+		}
+	}
+}
