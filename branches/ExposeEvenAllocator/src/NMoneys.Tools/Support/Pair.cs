9980a17cb1a037497778716ac5c652109dc4b6a1
danielgonnet@gmail.com <danielgonnet@gmail.com@8c90c430-1bf3-a179-f29b-9acbecd64b06>
2012-02-19 13:12:19 +0000

    what if scenario of changing IRemainderAllocator interface because EvenAllocator is surfaced

---
+++ branches/ExposeEvenAllocator/src/NMoneys.Tools/Support/Pair.cs
@@ -0,0 +1,14 @@
+﻿namespace NMoneys.Tools.Support
+{
+	public class Pair
+	{
+		public Pair(object first, object second)
+		{
+			First = first;
+			Second = second;
+		}
+
+		public object First { get; private set; }
+		public object Second { get; private set; }
+	}
+}
