0f56f9a6c2e2d368d981a8fd2d40bebbde7da0f1
danielgonnet@gmail.com <danielgonnet@gmail.com@8c90c430-1bf3-a179-f29b-9acbecd64b06>
2012-07-08 18:36:30 +0000

    added Money.arithmetic.operators

---
+++ src/NMoneys.Tests/MoneyTester.Arithmetic.Operators.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+
+namespace NMoneys.Tests
+{
+	[TestFixture]
+	public partial class MoneyTester
+	{
+		[Test]
+		public void UnaryNegate_AnotherMoneyWithNegativeAmount()
+		{
+			Money oweYouFive = -fiver;
+			Assert.That(oweYouFive, Is.Not.SameAs(fiver));
+			Assert.That(oweYouFive.Amount, Is.EqualTo(-(fiver.Amount)));
+			Assert.That(oweYouFive.CurrencyCode, Is.EqualTo(fiver.CurrencyCode));
+
+			Money youOweMeFive = -oweYouFive;
+			Assert.That(youOweMeFive.Amount, Is.EqualTo(fiver.Amount));
+		}
+
+		[Test]
+		public void Add_SameCurrency_AnotherMoneyWithAddedUpamount()
+		{
+			Money fifteenQuid = fiver + tenner;
+
+			Assert.That(fifteenQuid, Is.Not.SameAs(fiver).And.Not.SameAs(tenner));
+			Assert.That(fifteenQuid.Amount, Is.EqualTo(15));
+			Assert.That(fifteenQuid.CurrencyCode, Is.EqualTo(CurrencyIsoCode.GBP));
+		}
+
+		[Test]
+		public void Add_DifferentCurrency_Exception()
+		{
+			Money money;
+			Assert.That(() => money = fiver + hund, Throws.InstanceOf<DifferentCurrencyException>());
+		}
+
+		[Test]
+		public void Substract_SameCurrency_AnotherMoneyWithAddedUpamount()
+		{
+			Money anotherFiver = tenner - fiver;
+
+			Assert.That(anotherFiver, Is.Not.SameAs(fiver).And.Not.SameAs(tenner));
+			Assert.That(anotherFiver.Amount, Is.EqualTo(5));
+			Assert.That(anotherFiver.CurrencyCode, Is.EqualTo(CurrencyIsoCode.GBP));
+		}
+
+		[Test]
+		public void Substract_TennerMinusFiver_OweMeMoney()
+		{
+			Money oweFiver = fiver - tenner;
+			Assert.That(oweFiver.Amount, Is.EqualTo(-5));
+		}
+
+		[Test]
+		public void Substract_DifferentCurrency_Exception()
+		{
+			Money money;
+			Assert.That(() => money = fiver - hund, Throws.InstanceOf<DifferentCurrencyException>());
+		}
+	}
+}
