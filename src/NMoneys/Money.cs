76d751659e434af3f40eb01b5f3495134eeeb897
danielgonnet@gmail.com <danielgonnet@gmail.com@8c90c430-1bf3-a179-f29b-9acbecd64b06>
2012-07-08 18:54:55 +0000

    extracted serialization and xmlserialization

--- src/NMoneys/Money.cs
+++ src/NMoneys/Money.cs
@@ -2,14 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Globalization;
-using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Runtime.Serialization;
-using System.Security.Permissions;
-using System.Xml;
-using System.Xml.Schema;
-using System.Xml.Serialization;
 using NMoneys.Support;
 
 namespace NMoneys
@@ -17,9 +10,7 @@ namespace NMoneys
 	/// <summary>
 	/// A monetary quantity in a given currency
 	/// </summary>
-	[Serializable]
-	[XmlRoot(Namespace = Serialization.Data.NAMESPACE, ElementName = Serialization.Data.Money.ROOT_NAME, DataType = Serialization.Data.Money.DATA_TYPE, IsNullable = false)]
-	public partial struct Money : ISerializable, IXmlSerializable
+	public partial struct Money
 	{
 		#region .ctor
 
@@ -74,14 +65,6 @@ namespace NMoneys
 		{
 			setAllFields(money.Amount, money.CurrencyCode);
 		}
-
-		/// <summary>
-		/// Initializes a new instace of <see cref="Money"/> with serialized data
-		/// </summary>
-		/// <param name="info">The <see cref="SerializationInfo"/> that holds the serialized object data about the <see cref="Money"/>.</param>
-		/// <param name="context">The <see cref="StreamingContext"/> that contains contextual information about the source or destination.</param>
-		private Money(SerializationInfo info, StreamingContext context) :
-			this(info.GetDecimal(Serialization.Data.Money.AMOUNT), (CurrencyIsoCode)info.GetValue(Serialization.Data.Money.CURRENCY, typeof(CurrencyIsoCode))) { }
 
 		private void setAllFields(decimal amount, CurrencyIsoCode currency)
 		{
@@ -560,92 +543,6 @@ currency);
 			{
 				if (!HasSameCurrencyAs(money)) throw new DifferentCurrencyException(CurrencyCode.ToString(), money.CurrencyCode.ToString());	
 			}
-		}
-
-		#endregion
-
-		#region serialization
-
-		/// <summary>
-		/// Populates a <see cref="SerializationInfo"/> with the data needed to serialize the target object.
-		/// </summary>
-		/// <param name="info">The <see cref="System.Runtime.Serialization.SerializationInfo"/> to populate with data.</param>
-		/// <param name="context">The destination (see <see cref="T:System.Runtime.Serialization.StreamingContext"/>) for this serialization.</param>
-		/// <exception cref="T:System.Security.SecurityException">The caller does not have the required permission.</exception>
-		[SecurityPermission(SecurityAction.LinkDemand, Flags = SecurityPermissionFlag.SerializationFormatter)]
-		public void GetObjectData(SerializationInfo info, StreamingContext context)
-		{
-			info.AddValue(Serialization.Data.Money.AMOUNT, Amount);
-			info.AddValue(Serialization.Data.Money.CURRENCY, CurrencyCode, typeof(Currency));
-		}
-
-		/// <summary>
-		/// This method is reserved and should not be used.
-		/// When implementing the <see cref="IXmlSerializable"/> interface, you should return null from this method, and instead, if specifying a custom schema is required, apply the <see cref="T:System.Xml.Serialization.XmlSchemaProviderAttribute"/> to the class.
-		/// </summary>
-		/// <returns>null</returns>
-		[Obsolete("deprecated, use SchemaProviders instead")]
-		XmlSchema IXmlSerializable.GetSchema()
-		{
-			return null;
-		}
-
-		/// <summary>
-		/// Returns the XML schema applied for serialization.
-		/// </summary>
-		/// <param name="xs">A cache of XML Schema definition language (XSD) schemas.</param>
-		/// <returns>Represents the complexType element from XML Schema as specified by the <paramref name="xs"/>.</returns>
-		/// <exception cref="ArgumentNullException"><paramref name="xs"/> is null.</exception>
-		public static XmlSchemaComplexType GetSchema(XmlSchemaSet xs)
-		{
-			Guard.AgainstNullArgument("xs", xs);
-
-			XmlSchemaComplexType complex = null;
-			var schemaSerializer = new XmlSerializer(typeof(XmlSchema));
-			using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(Serialization.Data.ResourceName))
-			{
-				if (stream != null)
-				{
-					var schema = (XmlSchema)schemaSerializer.Deserialize(new XmlTextReader(stream));
-					xs.Add(schema);
-					var name = new XmlQualifiedName(Serialization.Data.Money.DATA_TYPE, Serialization.Data.NAMESPACE);
-					complex = (XmlSchemaComplexType)schema.SchemaTypes[name];
-				}
-			}
-			return complex;
-		}
-
-		/// <summary>
-		/// Generates an object from its XML representation.
-		/// </summary>
-		/// <param name="reader">The <see cref="XmlReader"/> stream from which the object is deserialized.</param>
-		/// <exception cref="ArgumentNullException"><paramref name="reader"/> is null.</exception>
-		public void ReadXml(XmlReader reader)
-		{
-			Guard.AgainstNullArgument("reader", reader);
-
-			reader.ReadStartElement();
-			setAllFields(reader.ReadElementContentAsDecimal(Serialization.Data.Money.AMOUNT, Serialization.Data.NAMESPACE),
-				Currency.ReadXmlData(reader));
-			reader.ReadEndElement();
-		}
-
-		/// <summary>
-		/// Converts an object into its XML representation.
-		/// </summary>
-		/// <param name="writer">The <see cref="XmlWriter"/> stream to which the object is serialized.</param>
-		/// <exception cref="ArgumentNullException"><paramref name="writer"/> is null.</exception>
-		public void WriteXml(XmlWriter writer)
-		{
-			Guard.AgainstNullArgument("writer", writer);
-
-			writer.WriteStartElement(Serialization.Data.Money.AMOUNT, Serialization.Data.NAMESPACE);
-			writer.WriteValue(Amount);
-			writer.WriteEndElement();
-			writer.WriteStartElement(Serialization.Data.Currency.ROOT_NAME);
-			Currency currency = Currency.Get(CurrencyCode);
-			currency.WriteXml(writer);
-			writer.WriteEndElement();
 		}
 
 		#endregion
