5cbfd7db308b6ce0ead88eea789efbf417d0e74e
danielgonnet@gmail.com <danielgonnet@gmail.com@8c90c430-1bf3-a179-f29b-9acbecd64b06>
2012-07-09 19:27:08 +0000

    updated version of boh packages and changed target to 3.5 client profile

--- branches/SupportClientProfile/src/NMoneys.Exchange/Properties/AssemblyInfo.cs
+++ branches/SupportClientProfile/src/NMoneys.Exchange/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NMoneys.Exchange")]
-[assembly: AssemblyDescription("Currency exhange capabilities for NMoneys")]
+[assembly: AssemblyDescription("Currency exchange capabilities for NMoneys")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NMoneys.Exchange")]
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("1.9.0.0")]
+[assembly: AssemblyFileVersion("1.9.0.0")]
 
 [assembly: CLSCompliant(true)]
