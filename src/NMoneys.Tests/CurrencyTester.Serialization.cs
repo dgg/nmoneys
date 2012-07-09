b17dd0dd518a69bab15c24bc47f69b9060293652
danielgonnet@gmail.com <danielgonnet@gmail.com@8c90c430-1bf3-a179-f29b-9acbecd64b06>
2012-07-09 14:30:14 +0000

    extracted currency responsibilities

---
+++ src/NMoneys.Tests/CurrencyTester.Serialization.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using NMoneys.Tests.CustomConstraints;
+using NMoneys.Tests.Support;
+using NUnit.Framework;
+
+namespace NMoneys.Tests
+{
+	[TestFixture]
+	public partial class CurrencyTester
+	{
+		[Test]
+		public void CanBe_BinarySerialized()
+		{
+			Assert.That(Currency.Dollar, Must.Be.BinarySerializable<Currency>(Is.EqualTo));
+		}
+
+		[Test]
+		public void BinaryDeserialization_OfObsoleteCurrency_RaisesEvent()
+		{
+			using (var serializer = new OneGoBinarySerializer<Currency>())
+			{
+				var obsolete = Currency.Get("EEK");
+				serializer.Serialize(obsolete);
+				Action deserializeObsolete = () => serializer.Deserialize();
+				Assert.That(deserializeObsolete, Must.RaiseObsoleteEvent.Once());
+			}
+		}
+
+		[Test]
+		public void BinaryDeserialization_DoesNotPreservesInstanceUniqueness()
+		{
+			using (var serializer = new OneGoBinarySerializer<Currency>())
+			{
+				Currency usd = Currency.Get("USD");
+				serializer.Serialize(usd);
+				Assert.That(serializer.Deserialize(), Is.EqualTo(usd));
+			}
+		}
+
+		[Test]
+		public void CannotBe_DataContractJsonSerialized()
+		{
+			Assert.That(Currency.Dollar, Must.Not.Be.DataContractJsonSerializable<Currency>());
+		}
+
+		[Test]
+		public void CanBe_JsonSerialized()
+		{
+			Assert.That(Currency.Dollar, Must.Be.JsonSerializable<Currency>(Is.SameAs));
+		}
+
+		[Test]
+		public void CanBe_JsonDeserializable()
+		{
+			string serializedDollar = "{\"isoCode\":\"USD\"}";
+			Assert.That(serializedDollar, Must.Be.JsonDeserializableInto(Currency.Dollar));
+		}
+
+		[Test]
+		public void JsonDeserialization_OfObsoleteCurrency_RaisesEvent()
+		{
+			using (var serializer = new OneGoJsonSerializer<Currency>())
+			{
+				var obsolete = Currency.Get("EEK");
+				serializer.Serialize(obsolete);
+				Action deserializeObsolete = () => serializer.Deserialize();
+				Assert.That(deserializeObsolete, Must.RaiseObsoleteEvent.Once());
+			}
+		}
+
+		[Test]
+		public void JsonDeserialization_DoesPreserveInstanceUniqueness()
+		{
+			using (var serializer = new OneGoJsonSerializer<Currency>())
+			{
+				Currency usd = Currency.Get("USD");
+				serializer.Serialize(usd);
+				Assert.That(serializer.Deserialize(), Is.SameAs(usd));
+			}
+		}
+
+		#region Issue 16. Case sensitivity. Currency instances can be obtained by any casing of the IsoCode (Alphbetic code)
+
+		[Test]
+		public void JsonDeserialization_IsCaseInsensitive()
+		{
+			string serializedDollar = "{\"isoCode\":\"uSd\"}";
+			Assert.That(serializedDollar, Must.Be.JsonDeserializableInto(Currency.Dollar));
+		}
+
+		#endregion
+	}
+}
---
+++ src/NMoneys/Currency.Equality.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace NMoneys
+{
+	public sealed partial class Currency : IEquatable<Currency>
+	{
+		/// <summary>
+		/// Indicates whether the current <see cref="Currency"/> instance is equal to another instance.
+		/// </summary>
+		/// <remarks>Only <see cref="IsoCode"/> is checked as the object cannot be mutated. For more thorough comparison use <see cref="CurrencyEqualityComparer"/></remarks>
+		/// <returns>
+		/// true if the current object is equal to the <paramref name="other"/> parameter; otherwise, false.
+		/// </returns>
+		/// <param name="other">A <see cref="Currency"/> to compare with this object.</param>
+		public bool Equals(Currency other)
+		{
+			if (ReferenceEquals(null, other)) return false;
+			if (ReferenceEquals(this, other)) return true;
+			// only IsoCode matters as it cannot be mutated
+			return Equals(other.IsoCode, IsoCode);
+		}
+
+		/// <summary>
+		/// Determines whether the specified <see cref="object"/> is equal to the current <see cref="Currency"/>.
+		/// </summary>
+		/// <returns>
+		/// true if the specified <see cref="object"/> is equal to the current <see cref="Currency"/>; otherwise, false.
+		/// </returns>
+		/// <param name="obj">The <see cref="object"/> to compare with the current <see cref="Currency"/>.</param> 
+		public override bool Equals(object obj)
+		{
+			if (ReferenceEquals(null, obj)) return false;
+			if (ReferenceEquals(this, obj)) return true;
+			if (obj.GetType() != typeof(Currency)) return false;
+			return Equals((Currency)obj);
+		}
+
+		/// <summary>
+		/// Serves as a hash function for a particular type. 
+		/// </summary>
+		/// <returns>
+		/// A hash code for the current <see cref="Currency"/>.
+		/// </returns>
+		public override int GetHashCode()
+		{
+			return IsoCode.GetHashCode();
+		}
+
+		///<summary>
+		/// Determines whether two specified currencies are equal.
+		///</summary>
+		///<param name="left">The first <see cref="Currency"/> to compare, or null.</param>
+		///<param name="right">The second <see cref="Currency"/> to compare, or null.</param>
+		///<returns>true if <paramref name="left"/> is equal to <paramref name="right"/>; otherwise, false.</returns>
+		public static bool operator ==(Currency left, Currency right)
+		{
+			return Equals(left, right);
+		}
+
+		///<summary>
+		/// Determines whether two specified currencies are not equal.
+		///</summary>
+		///<param name="left">The first <see cref="Currency"/> to compare, or null.</param>
+		///<param name="right">The second <see cref="Currency"/> to compare, or null.</param>
+		///<returns>true if <paramref name="left"/> is not equal to <paramref name="right"/>; otherwise, false.</returns>
+		public static bool operator !=(Currency left, Currency right)
+		{
+			return !Equals(left, right);
+		}
+	}
+}
