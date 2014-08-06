c071744e520fdc19cd93016a24e249a16c1667c1
danielgonnet@gmail.com <danielgonnet@gmail.com@8c90c430-1bf3-a179-f29b-9acbecd64b06>
2014-08-06 06:39:01 +0000

    add build-transformed package for RavenDB

--- trunk/src/NMoneys.Serialization/Json_NET/install.ps1
+++
@@ -1,8 +0,0 @@
-﻿param($installPath, $toolsPath, $package, $project)
-
-$projectPath = ([System.IO.Directory]::GetParent($project.FullName))
-$codePath = Join-Path -Path $projectPath -ChildPath "Infrastructure" | Join-Path -ChildPath "Serialization" | Join-Path -ChildPath "Json_NET.cs"
-
-(Get-Content $codePath) | 
-Foreach-Object {$_ -replace "Newtonsoft", "Raven.Imports.Newtonsoft"} | 
-Set-Content $codePath
