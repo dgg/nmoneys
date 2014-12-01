5273c26c03a2452adbb167111fd56d319462f206
danielgonnet@gmail.com <danielgonnet@gmail.com@8c90c430-1bf3-a179-f29b-9acbecd64b06>
2014-12-01 21:12:42 +0000

    added ServiceStack nullable money serialization support

--- src/NMoneys.Serialization.Tests/Service_Stack/DeserializationTester.cs
+++ src/NMoneys.Serialization.Tests/Service_Stack/DeserializationTester.cs
@@ -147,5 +147,203 @@ namespace NMoneys.Serialization.Tests.Service_Stack
 				Assert.That(actual.PropName, Is.EqualTo(expected));
 			}
 		}
+
+		#region nullable
+
+		[Test]
+		public void CustomCanonicalConverter_NotNullWithDefaultConfiguration_ReadsPascalCasedProperties()
+		{
+			Money? expected = new Money(14.3m, CurrencyIsoCode.XTS);
+
+			string json = "{\"Amount\":14.3,\"Currency\":{\"IsoCode\":\"XTS\"}}";
+
+			JsConfig<Money?>.RawDeserializeFn = CanonicalNullableMoneySerializer.Deserialize;
+
+			var actual = JsonSerializer.DeserializeFromString<Money?>(json);
+
+			JsConfig<Money?>.Reset();
+			Assert.That(actual, Is.EqualTo(expected));
+		}
+
+		[Test]
+		public void CustomCanonicalConverter_NullWithDefaultConfiguration_ReadsPascalCasedProperties()
+		{
+			string json = null;
+
+			JsConfig<Money?>.RawDeserializeFn = CanonicalNullableMoneySerializer.Deserialize;
+
+			var actual = JsonSerializer.DeserializeFromString<Money?>(json);
+
+			JsConfig<Money?>.Reset();
+			Assert.That(actual, Is.Null);
+		}
+
+		[Test]
+		public void CustomCanonicalConverter_NotNullWithCamelCasing_ReadsCamelCasedProperties()
+		{
+			Money? expected = new Money(14.3m, CurrencyIsoCode.XTS);
+
+			string json = "{\"amount\":14.3,\"currency\":{\"isoCode\":\"XTS\"}}";
+
+			using (new NullableConfigScope(CanonicalNullableMoneySerializer.Deserialize, JsConfig.With(emitCamelCaseNames: true)))
+			{
+				var actual = JsonSerializer.DeserializeFromString<Money?>(json);
+
+				Assert.That(actual, Is.EqualTo(expected));
+			}
+		}
+
+		[Test]
+		public void CustomCanonicalConverter_NullWithCamelCasing_ReadsCamelCasedProperties()
+		{
+			string json = null;
+
+			using (new NullableConfigScope(CanonicalNullableMoneySerializer.Deserialize, JsConfig.With(emitCamelCaseNames: true)))
+			{
+				var actual = JsonSerializer.DeserializeFromString<Money?>(json);
+
+				Assert.That(actual, Is.Null);
+			}
+		}
+
+		[Test]
+		public void CustomCanonicalConverter_NotNullContainer_ReadsCamelCasedProperties()
+		{
+			var expected = new Money(14.3m, CurrencyIsoCode.XTS);
+
+			string json = "{\"propName\":{\"amount\":14.3,\"currency\":{\"isoCode\":\"XTS\"}}}";
+
+			using (new NullableConfigScope(CanonicalNullableMoneySerializer.Deserialize, JsConfig.With(emitCamelCaseNames: true)))
+			{
+				var actual = JsonSerializer.DeserializeFromString<NullableMoneyContainer>(json);
+
+				Assert.That(actual.PropName, Is.EqualTo(expected));
+			}
+		}
+
+		[Test]
+		public void CustomCanonicalConverter_NullContainer_ReadsCamelCasedProperties()
+		{
+			string json = "{\"propName\":null}";
+
+			using (new NullableConfigScope(CanonicalNullableMoneySerializer.Deserialize, JsConfig.With(emitCamelCaseNames: true)))
+			{
+				var actual = JsonSerializer.DeserializeFromString<NullableMoneyContainer>(json);
+
+				Assert.That(actual.PropName, Is.Null);
+			}
+		}
+
+		[Test]
+		public void CustomDefaultConverter_NotNullWithDefaultConfiguration_ReadsPascalCasedProperties()
+		{
+			Money? expected = new Money(14.3m, CurrencyIsoCode.XTS);
+
+			string json = "{\"Amount\":14.3,\"Currency\":\"XTS\"}";
+
+			using (new NullableConfigScope(DefaultNullableMoneySerializer.Deserialize))
+			{
+				var actual = JsonSerializer.DeserializeFromString<Money?>(json);
+
+				Assert.That(actual, Is.EqualTo(expected));
+			}
+		}
+
+		[Test]
+		public void CustomDefaultConverter_NullWithDefaultConfiguration_ReadsPascalCasedProperties()
+		{
+			string json = null;
+
+			using (new NullableConfigScope(DefaultNullableMoneySerializer.Deserialize))
+			{
+				var actual = JsonSerializer.DeserializeFromString<Money?>(json);
+
+				Assert.That(actual, Is.Null);
+			}
+		}
+
+		[Test]
+		public void CustomDefaultConverter_NotNullWithCamelCase_ReadsCamelCasedProperties()
+		{
+			Money? expected = new Money(14.3m, CurrencyIsoCode.XTS);
+
+			string json = "{\"amount\":14.3,\"currency\":\"XTS\"}";
+			using (new NullableConfigScope(DefaultNullableMoneySerializer.Deserialize, JsConfig.With(emitCamelCaseNames: true)))
+			{
+				var actual = JsonSerializer.DeserializeFromString<Money?>(json);
+
+				Assert.That(actual, Is.EqualTo(expected));
+			}
+		}
+
+		[Test]
+		public void CustomDefaultConverter_NullWithCamelCase_ReadsCamelCasedProperties()
+		{
+			string json = null;
+			using (new NullableConfigScope(DefaultNullableMoneySerializer.Deserialize, JsConfig.With(emitCamelCaseNames: true)))
+			{
+				var actual = JsonSerializer.DeserializeFromString<Money?>(json);
+
+				Assert.That(actual, Is.Null);
+			}
+		}
+
+		[Test]
+		public void CustomDefaultConverter_NotNullNumericStyle_ReadsCurrencyAsNumber()
+		{
+			Money? expected = new Money(14.3m, CurrencyIsoCode.XTS);
+
+			string json = "{\"Amount\":14.3,\"Currency\":963}";
+
+			using (new NullableConfigScope(DefaultNullableMoneySerializer.Numeric.Deserialize, JsConfig.With(emitCamelCaseNames: false)))
+			{
+				var actual = JsonSerializer.DeserializeFromString<Money?>(json);
+
+				Assert.That(actual, Is.EqualTo(expected));
+			}
+		}
+
+		[Test]
+		public void CustomDefaultConverter_NullNumericStyle_ReadsCurrencyAsNumber()
+		{
+			string json = null;
+
+			using (new NullableConfigScope(DefaultNullableMoneySerializer.Numeric.Deserialize, JsConfig.With(emitCamelCaseNames: false)))
+			{
+				var actual = JsonSerializer.DeserializeFromString<Money?>(json);
+
+				Assert.That(actual, Is.Null);
+			}
+		}
+
+		[Test]
+		public void CustomDefaultConverter_NotNullContainer_ReadsCurrencyAsNumber()
+		{
+			var expected = new Money(14.3m, CurrencyIsoCode.XTS);
+
+			string json = "{\"PropName\":{\"Amount\":14.3,\"Currency\":963}}";
+
+			using (new NullableConfigScope(DefaultNullableMoneySerializer.Numeric.Deserialize, JsConfig.With(emitCamelCaseNames: false)))
+			{
+				var actual = JsonSerializer.DeserializeFromString<NullableMoneyContainer>(json);
+
+				Assert.That(actual.PropName, Is.EqualTo(expected));
+			}
+		}
+
+		[Test]
+		public void CustomDefaultConverter_NullContainer_ReadsCurrencyAsNumber()
+		{
+			string json = "{}";
+
+			using (new NullableConfigScope(DefaultNullableMoneySerializer.Numeric.Deserialize, JsConfig.With(emitCamelCaseNames: false)))
+			{
+				var actual = JsonSerializer.DeserializeFromString<NullableMoneyContainer>(json);
+
+				Assert.That(actual.PropName, Is.Null);
+			}
+		}
+
+		#endregion
 	}
 }
