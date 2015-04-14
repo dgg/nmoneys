7866ee8b31159ef35a7977be04df7aa5c4892b79
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2015-04-14 09:45:53 +0200

    add git ignores and fix missing artifacts

---
+++ src/NMoneys.Serialization.Tests/Service_Stack/Support/NullableConfigScope.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ServiceStack.Text;
+
+namespace NMoneys.Serialization.Tests.Service_Stack.Support
+{
+	public class NullableConfigScope : IDisposable
+	{
+		private readonly JsConfigScope _scope;
+
+		public NullableConfigScope(Func<Money?, string> serializer, JsConfigScope scope = null)
+		{
+			_scope = scope;
+			JsConfig<Money?>.RawSerializeFn = serializer;
+		}
+
+		public NullableConfigScope(Func<string, Money?> deSerializer, JsConfigScope scope = null)
+		{
+			_scope = scope;
+			JsConfig<Money?>.RawDeserializeFn = deSerializer;
+		}
+
+		public void Dispose()
+		{
+			if (_scope != null) _scope.Dispose();
+			JsConfig<Money?>.Reset();
+		}
+	}
+}
---
+++ src/NMoneys.Tests/Support/NullableMoneyContainer.cs
@@ -0,0 +1,7 @@
+namespace NMoneys.Tests.Support
+{
+	public class NullableMoneyContainer
+	{
+		public Money? PropName { get; set; }
+	}
+}
