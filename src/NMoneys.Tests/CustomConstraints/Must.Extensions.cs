2ce59c60dbd01cc801ae2583f4c879cbc860ba3c
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2015-09-24 10:46:15 +0200

    replace DataContractJsonSerializationConstraint

--- src/NMoneys.Tests/CustomConstraints/Must.Extensions.cs
+++ src/NMoneys.Tests/CustomConstraints/Must.Extensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using NMoneys.Serialization;
 using NUnit.Framework.Constraints;
 using Testing.Commons;
 using Testing.Commons.NUnit.Constraints;
@@ -40,12 +41,12 @@ namespace NMoneys.Tests.CustomConstraints
 
 		public static Constraint DataContractDeserializableInto<T>(this Must.BeEntryPoint entryPoint, T to)
 		{
-			return new DataContractDeserializationConstraint<T>(to);
+			return new DeserializationConstraint<T>(new DataContractDeserializer(), NUnit.Framework.Is.EqualTo(to));
 		}
 
 		public static Constraint DataContractJsonSerializable<T>(this Must.BeEntryPoint entryPoint)
 		{
-			return new DataContractJsonSerializationConstraint<T>();
+			return new SerializationConstraint<T>(new DataContractJsonRoundtripSerializer<T>(dataContractSurrogate: new DataContractSurrogate()), NUnit.Framework.Is.Not.Null);
 		}
 
 		public static Constraint DataContractJsonDeserializableInto<T>(this Must.BeEntryPoint entryPoint, T to)
