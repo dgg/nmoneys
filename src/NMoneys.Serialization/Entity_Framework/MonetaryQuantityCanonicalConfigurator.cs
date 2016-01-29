4763af9b14b3463d1a47ef296abe6bcae6ca8c4f
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2016-01-29 15:15:46 +0100

    add barebones version of EF-persistible MonetaryQuantity with tests using SQL Server Compact

---
+++ src/NMoneys.Serialization/Entity_Framework/MonetaryQuantityCanonicalConfigurator.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+
+namespace NMoneys.Serialization.Entity_Framework
+{
+	public static class MonetaryQuantityCanonicalConfigurator
+	{
+		public static void Configure(DbModelBuilder modelBuilder)
+		{
+			modelBuilder.ComplexType<MonetaryQuantity>()
+				.Property(c => c.Currency)
+				.IsFixedLength().HasMaxLength(3);
+
+			modelBuilder.ComplexType<MonetaryQuantity>()
+				.Property(c => c.Amount).HasPrecision(19, 4);
+		}
+
+		public static void ConfigureMonetaryQuantity(this DbModelBuilder modelBuilder)
+		{
+			Configure(modelBuilder);
+		}
+	}
+}
--- src/NMoneys.Serialization/Properties/AssemblyInfo.cs
+++ src/NMoneys.Serialization/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NMoneys.Serialization.Json_NET")]
+[assembly: AssemblyTitle("NMoneys.Serialization")]
 [assembly: AssemblyDescription("Custom serialization for Money Value Object related objects.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
