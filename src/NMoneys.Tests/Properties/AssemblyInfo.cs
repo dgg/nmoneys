30c4a75935970bc9f84c4ccfa8ce3c7c374339d7
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2016-03-16 10:25:07 +0100

    closes #162 and introduce shared versioning to NMoneys

--- src/NMoneys.Tests/Properties/AssemblyInfo.cs
+++ src/NMoneys.Tests/Properties/AssemblyInfo.cs
@@ -7,32 +7,9 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("NMoneys.Tests")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NMoneys.Tests")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("63090818-1cdd-46c6-b835-304183b85319")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.1.0.0")]
-[assembly: AssemblyFileVersion("4.1.0.0")]
 
 [assembly: InternalsVisibleTo("NMoneys.Exchange.Tests")]
 [assembly: InternalsVisibleTo("NMoneys.Serialization.Tests")]
--- src/NMoneys/Properties/AssemblyInfo.cs
+++ src/NMoneys/Properties/AssemblyInfo.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Reflection;
-using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -9,32 +8,9 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("NMoneys")]
 [assembly: AssemblyDescription("Implementation of the Money Value Object to support representing moneys in the currencies defined in the ISO 4217 standard.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NMoneys")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6008a10f-70bd-400b-8ebb-112069622351")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.1.0.0")]
-[assembly: AssemblyFileVersion("4.1.0.0")]
 
 [assembly: InternalsVisibleTo("NMoneys.Tests")]
 [assembly: InternalsVisibleTo("NMoneys.Tools")]
