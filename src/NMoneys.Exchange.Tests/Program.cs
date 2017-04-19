43b17aab8f249a93e2c22c098fe9e2e257b80000
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2017-04-19 14:17:26 +0200

    migrated nmoneys.exchange

---
+++ src/NMoneys.Exchange.Tests/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using NUnit.Common;
+using NUnitLite;
+
+namespace NMoneys.Exchange.Tests
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var writter = new ExtendedTextWrapper(Console.Out);
+			int exitCode = new AutoRun(typeof(Program).GetTypeInfo().Assembly).Execute(args, writter, Console.In);
+			Environment.Exit(exitCode);
+		}
+	}
+}
