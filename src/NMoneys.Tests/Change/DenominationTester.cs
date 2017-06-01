c749b26400b909b35c1fcbee40cb54821b7c6342
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2017-06-01 11:52:14 +0200

    we need to be able to create money denominations

---
+++ src/NMoneys.Tests/Change/DenominationTester.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace NMoneys.Tests.Change
+{
+	[TestFixture]
+	public class DenominationTester
+	{
+		[Test]
+		public void Ctor_WithValue()
+		{
+			var subject = new Denomination(5m);
+
+			Assert.That(subject.Value, Is.EqualTo(5m));
+		}
+	}
+}
