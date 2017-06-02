3519afdc9628b130ad05d062951ac69bda0e9aca
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2017-06-02 08:53:49 +0200

    positive denominations

--- src/NMoneys/Change/Denomination.cs
+++ src/NMoneys/Change/Denomination.cs
@@ -5,7 +5,7 @@ namespace NMoneys.Change
 	public class Denomination
 	{
 		private static readonly Range<decimal> _range = new Range<decimal>(
-			decimal.Zero.Close(), decimal.MaxValue.Close());
+			decimal.Zero.Open(), decimal.MaxValue.Close());
 		public Denomination(decimal value)
 		{
 			_range.AssertArgument(nameof(value), value);
