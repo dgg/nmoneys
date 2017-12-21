b6f3f8d371e4412c3acb91f49252feaa13e02861
Daniel González García <danielgonnet@gmail.com>
2017-12-21 21:44:41 +0100

    add shortcut to copy cultureinfo property

---
+++ src/NMoneys.Tools/Clip-CI-Property.ps1
@@ -0,0 +1,14 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory = $true, HelpMessage = "Culture to copy information from")]
+    [String]$CultureName,
+    [Parameter(HelpMessage = "Property container from CultureInfo")]
+    [string]$Container = 'NumberFormat',
+    [Parameter(HelpMessage = "Property from CultureInfo's container property")]
+    [String]$Property = 'CurrencyGroupSeparator'
+)
+$ci = [System.Globalization.CultureInfo]::GetCultureInfo($CultureName)
+
+$value = $ci.$Container.$Property
+[System.Windows.Forms.Clipboard]::SetText($value)
+Write-Host "Copied '$value' to clipboard"
