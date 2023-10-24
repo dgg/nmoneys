a30d7941fbacce0f694217e4d43674ec53186f25
Daniel González García <danielgonnet@gmail.com>
2023-10-24 20:42:41 +0200

    moved to .old and started anew

---
+++ tests/NMoneys.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace NMoneys.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
