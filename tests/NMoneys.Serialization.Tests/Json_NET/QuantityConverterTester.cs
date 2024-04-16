6034e1b0e7c436802809f2035cf79e05525996c8
Daniel González García <danielgonnet@gmail.com>
2024-04-16 15:24:22 +0200

    fix tests

--- tests/NMoneys.Serialization.Tests/Json_NET/QuantityConverterTester.cs
+++ tests/NMoneys.Serialization.Tests/Json_NET/QuantityConverterTester.cs
@@ -15,84 +15,57 @@ public class QuantityConverterTester
 	# region serialization
 
 	[Test]
-	public void Serialize_DefaultResolver_PascalCasedAndNumericCurrency()
+	public void Serialize_DefaultResolver_QuantityString()
 	{
 		var toSerialize = new Money(14.3m, CurrencyIsoCode.XTS);
 
 		var subject = new QuantityConverter();
 		string actual = JsonConvert.SerializeObject(toSerialize, subject);
 
-		Assert.That(actual, Is.EqualTo("{'Amount':14.3,'Currency':963}".Jsonify()));
+		Assert.That(actual, Is.EqualTo("'XTS 14.3'".Jsonify()));
 	}
 
 	[Test]
-	public void Serialize_DefaultResolverWithForcedStringCurrency_PascalCasedAndStringCurrency()
+	public void Serialize_CustomResolverAndEnumFormat_ConfigurationDoesNotApply()
 	{
 		var toSerialize = new Money(14.3m, CurrencyIsoCode.XTS);
 
-		var subject = new MoneyConverter(true);
-		string actual = JsonConvert.SerializeObject(toSerialize, subject);
-
-		Assert.That(actual, Is.EqualTo("{'Amount':14.3,'Currency':'XTS'}".Jsonify()));
-	}
-
-	[Test]
-	public void Serialize_CustomResolverAndEnumFormat_FollowsCaseAndCurrencyFormat()
-	{
-		var toSerialize = new Money(14.3m, CurrencyIsoCode.XTS);
-
-		var subject = new MoneyConverter();
+		var subject = new QuantityConverter();
 		var settings = new JsonSerializerSettings
 		{
 			ContractResolver = new CamelCasePropertyNamesContractResolver(),
-			Converters = new JsonConverter[] { new StringEnumConverter(), subject }
+			Converters = [new StringEnumConverter(), subject]
 		};
 		string actual = JsonConvert.SerializeObject(toSerialize, settings);
 
-		Assert.That(actual, Is.EqualTo("{'amount':14.3,'currency':'XTS'}".Jsonify()));
-	}
-
-	[Test]
-	public void Serialize_CustomResolverAndForcedString_FollowsCaseAndOverridesCurrencyFormat()
-	{
-		var toSerialize = new Money(14.3m, CurrencyIsoCode.XTS);
-
-		var subject = new MoneyConverter(true);
-		var settings = new JsonSerializerSettings
-		{
-			ContractResolver = new CamelCasePropertyNamesContractResolver(),
-			Converters = new JsonConverter[] { subject }
-		};
-		string actual = JsonConvert.SerializeObject(toSerialize, settings);
-
-		Assert.That(actual, Is.EqualTo("{'amount':14.3,'currency':'XTS'}".Jsonify()));
+		Assert.That(actual, Is.EqualTo("'XTS 14.3'".Jsonify()));
 	}
 
 	[Test]
 	public void Serialize_Null_DefaultNullable()
 	{
-		Money? @null = default(Money?);
+		Money? @null = default;
 
-		var subject = new MoneyConverter();
+		var subject = new QuantityConverter();
 
 		string actual = JsonConvert.SerializeObject(@null, subject);
 		Assert.That(actual, Is.EqualTo("null"));
 	}
 
 	[Test]
-	public void Serialize_DefaultResolverRecord_PascalCasedAndNumericCurrency()
+	public void Serialize_DefaultResolverRecord_QuantityString()
 	{
 		var money = new Money(14.3m, CurrencyIsoCode.XTS);
 		var toSerialize = new MoneyRecord("s", money, 42);
 
-		var subject = new MoneyConverter();
+		var subject = new QuantityConverter();
 		string actual = JsonConvert.SerializeObject(toSerialize, subject);
 
-		Assert.That(actual, Is.EqualTo("{'S':'s','M':{'Amount':14.3,'Currency':963},'N':42}".Jsonify()));
+		Assert.That(actual, Is.EqualTo("{'S':'s','M':'XTS 14.3','N':42}".Jsonify()));
 	}
 
 	[Test]
-	public void Serialize_CustomContainer_FollowsConfiguration()
+	public void Serialize_CustomContainer_ConfigurationDoesNotApply()
 	{
 		var money = new Money(14.3m, CurrencyIsoCode.XTS);
 		var toSerialize = new MoneyContainer
@@ -102,15 +75,15 @@ public class QuantityConverterTester
 			N = 42
 		};
 
-		var subject = new MoneyConverter();
+		var subject = new QuantityConverter();
 		var settings = new JsonSerializerSettings
 		{
 			ContractResolver = new CamelCasePropertyNamesContractResolver(),
-			Converters = new JsonConverter[] { new StringEnumConverter(), subject }
+			Converters = [new StringEnumConverter(), subject]
 		};
 		string actual = JsonConvert.SerializeObject(toSerialize, settings);
 
-		Assert.That(actual, Is.EqualTo("{'s':'s','m':{'amount':14.3,'currency':'XTS'},'n':42}".Jsonify()));
+		Assert.That(actual, Is.EqualTo("{'s':'s','m':'XTS 14.3','n':42}".Jsonify()));
 	}
 
 	[Test]
@@ -121,7 +94,7 @@ public class QuantityConverterTester
 			N = 42
 		};
 
-		var subject = new MoneyConverter();
+		var subject = new QuantityConverter();
 		string actual = JsonConvert.SerializeObject(toSerialize, subject);
 
 		Assert.That(actual, Is.EqualTo("{'S':null,'M':null,'N':42}".Jsonify()));
@@ -132,11 +105,11 @@ public class QuantityConverterTester
 	{
 		var toSerialize = new NullableMoneyRecord(null, null, 42);
 
-		var subject = new MoneyConverter();
+		var subject = new QuantityConverter();
 		var settings = new JsonSerializerSettings
 		{
 			ContractResolver = new CamelCasePropertyNamesContractResolver(),
-			Converters = new JsonConverter[] { new StringEnumConverter(), subject }
+			Converters = [new StringEnumConverter(), subject]
 		};
 		string actual = JsonConvert.SerializeObject(toSerialize, settings);
 
@@ -148,12 +121,12 @@ public class QuantityConverterTester
 	#region deserialization
 
 	[Test]
-	public void Deserialize_DefaultResolver_PascalCasedAndNumericCurrency()
+	public void Deserialize_DefaultResolver_QuantityString()
 	{
-		string json = "{'Amount':14.3,'Currency':963}".Jsonify();
+		string json = "'XTS 14.3'".Jsonify();
 		var expected = new Money(14.3m, CurrencyIsoCode.XTS);
 
-		var subject = new MoneyConverter();
+		var subject = new QuantityConverter();
 		Money deserialized = JsonConvert.DeserializeObject<Money>(json, subject);
 
 		Assert.That(deserialized, Is.EqualTo(expected));
@@ -164,7 +137,7 @@ public class QuantityConverterTester
 	{
 		var original = new Money(14.3m, CurrencyIsoCode.XTS);
 
-		var subject = new MoneyConverter();
+		var subject = new QuantityConverter();
 		string json = JsonConvert.SerializeObject(original, subject);
 		var deserialized = JsonConvert.DeserializeObject<Money>(json, subject);
 
@@ -172,40 +145,16 @@ public class QuantityConverterTester
 	}
 
 	[Test]
-	public void Deserialize_DefaultResolverWithForcedStringCurrency_PascalCasedAndStringCurrency()
+	public void Deserialize_CustomResolverAndEnumFormat_IgnoresConfig()
 	{
-		string json = "{'Amount':14.3,'Currency':'XTS'}".Jsonify();
+		string json = "'963 14.3'".Jsonify();
 		var expected = new Money(14.3m, CurrencyIsoCode.XTS);
 
-		var subject = new MoneyConverter(true);
-		Money deserialized = JsonConvert.DeserializeObject<Money>(json, subject);
-
-		Assert.That(deserialized, Is.EqualTo(expected));
-	}
-
-	[Test]
-	public void Deserialize_DefaultResolverWithForcedStringCurrency_CanRoundtrip()
-	{
-		var original = new Money(14.3m, CurrencyIsoCode.XTS);
-
-		var subject = new MoneyConverter(true);
-		string json = JsonConvert.SerializeObject(original, subject);
-		var deserialized = JsonConvert.DeserializeObject<Money>(json, subject);
-
-		Assert.That(deserialized, Is.EqualTo(original));
-	}
-
-	[Test]
-	public void Deserialize_CustomResolverAndEnumFormat_FollowsCaseAndCurrencyFormat()
-	{
-		string json = "{'amount':14.3,'currency':'XTS'}".Jsonify();
-		var expected = new Money(14.3m, CurrencyIsoCode.XTS);
-
-		var subject = new MoneyConverter();
+		var subject = new QuantityConverter();
 		var settings = new JsonSerializerSettings
 		{
 			ContractResolver = new CamelCasePropertyNamesContractResolver(),
-			Converters = new JsonConverter[] { new StringEnumConverter(), subject }
+			Converters = [new StringEnumConverter(), subject]
 		};
 		var actual = JsonConvert.DeserializeObject<Money>(json, settings);
 
@@ -217,11 +166,11 @@ public class QuantityConverterTester
 	{
 		var original = new Money(14.3m, CurrencyIsoCode.XTS);
 
-		var subject = new MoneyConverter();
+		var subject = new QuantityConverter();
 		var settings = new JsonSerializerSettings
 		{
 			ContractResolver = new CamelCasePropertyNamesContractResolver(),
-			Converters = new JsonConverter[] { new StringEnumConverter(), subject }
+			Converters = [new StringEnumConverter(), subject]
 		};
 		string json = JsonConvert.SerializeObject(original, settings);
 		var deserialized = JsonConvert.DeserializeObject<Money>(json, settings);
@@ -230,43 +179,9 @@ public class QuantityConverterTester
 	}
 
 	[Test]
-	public void Deserialize_CustomResolverAndForcedString_FollowsCaseAndOverridesCurrencyFormat()
-	{
-		string json = "{'amount':14.3,'currency':'XTS'}".Jsonify();
-		var expected = new Money(14.3m, CurrencyIsoCode.XTS);
-
-		var subject = new MoneyConverter(true);
-		var settings = new JsonSerializerSettings
-		{
-			ContractResolver = new CamelCasePropertyNamesContractResolver(),
-			Converters = new JsonConverter[] { subject }
-		};
-		var actual = JsonConvert.DeserializeObject<Money>(json,  settings);
-
-		Assert.That(actual, Is.EqualTo(expected));
-	}
-
-	[Test]
-	public void Deserialize_CustomResolverAndForcedString_CanRoundtrip()
-	{
-		var original = new Money(14.3m, CurrencyIsoCode.XTS);
-
-		var subject = new MoneyConverter(true);
-		var settings = new JsonSerializerSettings
-		{
-			ContractResolver = new CamelCasePropertyNamesContractResolver(),
-			Converters = new JsonConverter[] { subject }
-		};
-		var json = JsonConvert.SerializeObject(original,  settings);
-		var deserialized = JsonConvert.DeserializeObject<Money>(json, settings);
-
-		Assert.That(deserialized, Is.EqualTo(original));
-	}
-
-	[Test]
 	public void Deserialize_Null_DefaultNullable()
 	{
-		var subject = new MoneyConverter();
+		var subject = new QuantityConverter();
 
 		var actual = JsonConvert.DeserializeObject<Money?>("null", subject);
 		Assert.That(actual, Is.Null);
@@ -275,11 +190,11 @@ public class QuantityConverterTester
 	[Test]
 	public void Deserialize_DefaultResolverRecord_PascalCasedAndNumericCurrency()
 	{
-		string json = "{'S':'s','M':{'Amount':14.3,'Currency':963},'N':42}".Jsonify();
+		string json = "{'S':'s','M':'XTS 14.3','N':42}".Jsonify();
 		var money = new Money(14.3m, CurrencyIsoCode.XTS);
 		var expected = new MoneyRecord("s", money, 42);
 
-		var subject = new MoneyConverter();
+		var subject = new QuantityConverter();
 		MoneyRecord? actual = JsonConvert.DeserializeObject<MoneyRecord>(json, subject);
 
 		Assert.That(actual, Is.EqualTo(expected));
@@ -288,14 +203,14 @@ public class QuantityConverterTester
 	[Test]
 	public void Deserialize_CustomContainer_FollowsConfiguration()
 	{
-		string json = "{'s':'s','m':{'amount':14.3,'currency':'XTS'},'n':42}".Jsonify();
+		string json = "{'s':'s','m':'XTS 14.3','n':42}".Jsonify();
 		var money = new Money(14.3m, CurrencyIsoCode.XTS);
 
-		var subject = new MoneyConverter();
+		var subject = new QuantityConverter();
 		var settings = new JsonSerializerSettings
 		{
 			ContractResolver = new CamelCasePropertyNamesContractResolver(),
-			Converters = new JsonConverter[] { new StringEnumConverter(), subject }
+			Converters = [new StringEnumConverter(), subject]
 		};
 		var actual = JsonConvert.DeserializeObject<MoneyContainer>(json, settings);
 
@@ -310,7 +225,7 @@ public class QuantityConverterTester
 	{
 		string json = "{'S':null,'M':null,'N':42}".Jsonify();
 
-		var subject = new MoneyConverter();
+		var subject = new QuantityConverter();
 		var actual = JsonConvert.DeserializeObject<NullableMoneyContainer>(json, subject);
 
 		Assert.That(actual, Has.Property("S").Null);
@@ -324,11 +239,11 @@ public class QuantityConverterTester
 		string json = "{'s':null,'m':null,'n':42}".Jsonify();
 		var expected = new NullableMoneyRecord(null, null, 42);
 
-		var subject = new MoneyConverter();
+		var subject = new QuantityConverter();
 		var settings = new JsonSerializerSettings
 		{
 			ContractResolver = new CamelCasePropertyNamesContractResolver(),
-			Converters = new JsonConverter[] { new StringEnumConverter(), subject }
+			Converters = [new StringEnumConverter(), subject]
 		};
 		var actual = JsonConvert.DeserializeObject<NullableMoneyRecord>(json, settings);
 
@@ -341,42 +256,37 @@ public class QuantityConverterTester
 	public void Deserialize_DefaultNotAnObject_Exception()
 	{
 		string notAJsonObject = "'str'".Jsonify();
-		var subject = new MoneyConverter();
+		var subject = new QuantityConverter();
 
 		Assert.That(()=> JsonConvert.DeserializeObject<Money>(notAJsonObject, subject),
-			Throws.InstanceOf<JsonSerializationException>()
-				.With.Message.Contains("token 'Object'")
-				.And.Message.Contains("'String'"));
+			Throws.ArgumentException);
 	}
 
 	[Test]
 	public void Deserialize_DefaultMissingAmount_Exception()
 	{
-		string missingAmount = "{}".Jsonify();
-		var subject = new MoneyConverter();
+		string missingAmount = "'XTS '".Jsonify();
+		var subject = new QuantityConverter();
 
 		Assert.That(()=> JsonConvert.DeserializeObject<Money>(missingAmount, subject),
-			Throws.InstanceOf<JsonSerializationException>()
-				.With.Message.Contains("Missing property")
-				.And.Message.Contains("'Amount'"));
+			Throws.InstanceOf<FormatException>());
 	}
 
 	[Test]
 	public void Deserialize_DefaultNonNumericAmount_Exception()
 	{
-		string nonNumericAmount = "{'Amount':'lol'}".Jsonify();
-		var subject = new MoneyConverter();
+		string nonNumericAmount = "'XTS 'lol''".Jsonify();
+		var subject = new QuantityConverter();
 
 		Assert.That(()=> JsonConvert.DeserializeObject<Money>(nonNumericAmount, subject),
-			Throws.InstanceOf<FormatException>()
-				.With.Message.Contains("input string 'lol'"));
+			Throws.InstanceOf<FormatException>());
 	}
 
 	[Test]
 	public void Deserialize_CaseDoesNotMatchContract_DoesntMatter()
 	{
-		string caseMismatch = "{'Amount':1,'currency':963}".Jsonify();
-		var subject = new MoneyConverter();
+		string caseMismatch = "'963 1'".Jsonify();
+		var subject = new QuantityConverter();
 
 		Assert.That(()=> JsonConvert.DeserializeObject<Money>(caseMismatch, subject),
 			Throws.Nothing);
@@ -385,10 +295,10 @@ public class QuantityConverterTester
 	}
 
 	[Test]
-	public void Deserialize_MissingCurrency_ExceptionIgnoringCasing()
+	public void Deserialize_MissingCurrency_Exception()
 	{
-		string missingCurrency = "{'amount':1}".Jsonify();
-		var subject = new MoneyConverter();
+		string missingCurrency = "'1'".Jsonify();
+		var subject = new QuantityConverter();
 
 		var settings = new JsonSerializerSettings
 		{
@@ -396,16 +306,14 @@ public class QuantityConverterTester
 			Converters = { subject }
 		};
 		Assert.That(()=> JsonConvert.DeserializeObject<Money>(missingCurrency, settings),
-			Throws.InstanceOf<JsonSerializationException>()
-				.With.Message.Contains("Missing property")
-				.And.Message.Contains("'Currency'"), "no camel-casing");
+			Throws.InstanceOf<ArgumentOutOfRangeException>());
 	}
 
 	[Test]
-	public void Deserialize_MismatchCurrencyFormat_DoesNotMatter()
+	public void Deserialize_MismatchCurrencyFormat_MattersMuch()
 	{
-		string currencyFormatMismatch = "{'amount':1,'currency':'XTS'}".Jsonify();
-		var subject = new MoneyConverter();
+		string currencyFormatMismatch = "'xts 1'".Jsonify();
+		var subject = new QuantityConverter();
 
 		var settings = new JsonSerializerSettings
 		{
@@ -413,17 +321,14 @@ public class QuantityConverterTester
 			Converters = { subject }
 		};
 		Assert.That(()=> JsonConvert.DeserializeObject<Money>(currencyFormatMismatch, settings),
-			Throws.Nothing);
-
-		Money deserialized = JsonConvert.DeserializeObject<Money>(currencyFormatMismatch, settings);
-		Assert.That(deserialized, Is.EqualTo(1m.Xts()));
+			Throws.ArgumentException);
 	}
 
 	[Test]
 	public void Deserialize_FunkyCurrencyValueCasing_DoesNotMatter()
 	{
-		string funkyCurrencyValue = "{'amount':1,'currency':'XtS'}".Jsonify();
-		var subject = new MoneyConverter();
+		string funkyCurrencyValue = "'XtS 1'".Jsonify();
+		var subject = new QuantityConverter();
 
 		var settings = new JsonSerializerSettings
 		{
@@ -431,17 +336,14 @@ public class QuantityConverterTester
 			Converters = { subject }
 		};
 		Assert.That(()=> JsonConvert.DeserializeObject<Money>(funkyCurrencyValue, settings),
-			Throws.Nothing);
-
-		Money deserialized = JsonConvert.DeserializeObject<Money>(funkyCurrencyValue, settings);
-		Assert.That(deserialized, Is.EqualTo(1m.Xts()));
+			Throws.ArgumentException);
 	}
 
 	[Test]
 	public void Deserialize_UndefinedCurrency_Exception()
 	{
-		string undefinedCurrency = "{'amount':1,'currency':1}".Jsonify();
-		var subject = new MoneyConverter();
+		string undefinedCurrency = "'AAA 1'".Jsonify();
+		var subject = new QuantityConverter();
 
 		var settings = new JsonSerializerSettings
 		{
@@ -449,14 +351,14 @@ public class QuantityConverterTester
 			Converters = { subject }
 		};
 		Assert.That(()=> JsonConvert.DeserializeObject<Money>(undefinedCurrency, settings),
-			Throws.InstanceOf<UndefinedCodeException>());
+			Throws.ArgumentException);
 	}
 
 	[Test]
 	public void Deserialize_PoorlyConstructedJson_Exception()
 	{
-		string missingObjectClose = "{'amount':1,'currency':1".Jsonify();
-		var subject = new MoneyConverter();
+		string missingObjectClose = "'XTS 1".Jsonify();
+		var subject = new QuantityConverter();
 
 		Assert.That(()=> JsonConvert.DeserializeObject<Money>(missingObjectClose, subject),
 			Throws.InstanceOf<JsonReaderException>());
